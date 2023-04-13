c3ba8190b1641b8d2b3196fd9063c472762c2737
albrechtno <noah.albrecht@ict-campus.net>
2023-04-13 13:34:43 +0200

    I added the earth and water element to the sword.

--- src/main/java/net/lelan/myrtenastermod/item/custom/MyrtenasterItem.java
+++ src/main/java/net/lelan/myrtenastermod/item/custom/MyrtenasterItem.java
@@ -1,11 +1,16 @@
 package net.lelan.myrtenastermod.item.custom;
 
 import net.lelan.myrtenastermod.effect.ModEffects;
+import net.lelan.myrtenastermod.event.ModEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.level.Level;
 
 public class MyrtenasterItem extends SwordItem {
 
@@ -22,4 +27,22 @@ public class MyrtenasterItem extends SwordItem {
 		}
 		return super.hurtEnemy(pItemStack, pTarget, pAttacker);
 	}
+
+	@Override
+	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
+		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
+		ItemStack itemstack = ar.getObject();
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+		if(current_element == "earth"){
+			ModEvents.earthExecute(world, entity);
+		}else if(current_element == "water"){
+			ModEvents.waterExecute(world, entity, itemstack);
+		}
+		return ar;
+	}
+
+
+
 }
